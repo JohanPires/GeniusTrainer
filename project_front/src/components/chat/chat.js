4ff148bd551b07e7ffef4d863394afa3836f9f9a
JohanPires <johan2408.jp@gmail.com>
2024-09-30 16:35:25 +0200

    optimisation blade

--- project_front/src/components/chat/chat.js
+++ project_front/src/components/chat/chat.js
@@ -44,7 +44,7 @@ function Chat() {
   };
 
   return (
-    <div className="chat ">
+    <div className="chat">
       <div className="searchBar w-4/5 sm:w-2/5 mx-auto my-5  flex flex-col gap-2">
         <div className="shadow-md p-5 rounded-full flex gap-2">
           <label htmlFor="search">
